3def1738341ee8c3baedee9c463d8a1a0e6a87fd
Jit Sarkar <jit.sarkar25@gmail.com>
2018-02-12 20:36:02 +0530

    Update appmain.js

--- appmain.js
+++ appmain.js
@@ -1,6 +1,6 @@
 
 var xstart=20;
-var ystart=148;
+//ystart=gapshelf+20; //0 to start from border,20 to start from shelf formula(173-height)
 var xend=280;
 var yend=500;
 var width=60;
@@ -8,20 +8,24 @@ var height = 30;
 var row=0;
 var column=0;
 var remaining=280;
-var isfull=false;
+isfull=false;
 var isfirst=true;
 var noOfProducts=0;
 var whereToSearch=0;
+
+ yOfLastRow=0;
+ currentRow=0;
+var lastRow=0;
 /*var pic= new Image();
 pic.src="aquariumfish.jpg";
 
 var pic2= new Image();
 pic2.src="aquariumfish.jpg";*/
 
-var src1="red.jpg";
-var src2="blue.png";
+var src1="boost.jpg";
+var src2="horlicks.png";
 
-var imageObjs=[];
+imageObjs=[];
 var shelf=[];
 function doFirst2(){
 	x= document.getElementById("shelfcanvas");
@@ -30,8 +34,8 @@ function doFirst2(){
 	mypic.addEventListener("dragstart",function (e){
 
 		e.dataTransfer.setData('picsent',src1);
-		e.dataTransfer.setData('width',35);
-		e.dataTransfer.setData('height',30);
+		e.dataTransfer.setData('width',45);
+		e.dataTransfer.setData('height',75);
 }, false);
 	
 	mypic2 = document.getElementById("img2");
@@ -39,7 +43,7 @@ function doFirst2(){
 
 		e.dataTransfer.setData('picsent',src2);
 		e.dataTransfer.setData('width',35);
-		e.dataTransfer.setData('height',30);
+		e.dataTransfer.setData('height',80);
 }, false);
 	//imageObjs.push(shelf);
 	leftbox = document.getElementById("leftbox");
@@ -65,7 +69,7 @@ function dropped(e){
 		width = parseInt(e.dataTransfer.getData('width'));
 		height = parseInt(e.dataTransfer.getData('height'));
 		
-	
+		console.log('ystart '+ystart);
 	noOfProducts++;
 if(isfirst){
 	xstart=20;
@@ -77,12 +81,13 @@ if(isfirst){
 else{
 		if(remaining < width)
 				{
-					if(ystart + height >= yend-height){
+					//if(yOfLastRow + height >= yend-height){
+					if(currentRow==lastRow){
 						isfull=true;
 					}
 					else{
 					//imageObjs.push([]);
-					ystart+=25+153-30;
+					//ystart+=gapsshelf+10;
 					remaining=280-width;
 					console.log('xstart = 0');
 					xstart=20;
@@ -111,10 +116,11 @@ else{
 			row : row,
 			column : column,
 			//xstart : xstart,
-			ystart : ystart,
+			ystart : ystart-height,
 			width : width,
 			height : height,
-			xstopped: e.pageX
+			xstopped: e.pageX,
+			ystopped: e.pageY
 			
 		};
 			//imageObjs[row].push(img);
@@ -122,17 +128,9 @@ else{
 			canvas.clearRect(0,0,300,500);
 			doFirst(shelves);
 			manipulateImages();
-			for(var i = 0 ; i <imageObjs.length; i++){
 			
-			for(var j = 0 ; j < imageObjs[i].length;j++){
-		//	console.log(imageObjs[i][j].xstart+'   '+imageObjs[i][j].ystart);
-			var pic = new Image();
-			pic.src = imageObjs[i][j].src;
-			
-			canvas.drawImage(pic,imageObjs[i][j].xfinal,imageObjs[i][j].ystart,imageObjs[i][j].width,imageObjs[i][j].height);
-			
-				}
-				}
+			//make another function 
+			placeOnShelf();
 				
 				
 		//leftbox.innerHTML = e.dataTransfer.getData('Text');
@@ -141,6 +139,48 @@ else{
 alert('Full');
 }
 }
+
+function placeOnShelf(){
+			lastRow=shelves;
+			currentRow=imageObjs.length;
+		    for(var i = 0 ; i <imageObjs.length; i++){
+			
+		
+			for(var j = 0 ; j < imageObjs[i].length;j++){
+		//	console.log(imageObjs[i][j].xstart+'   '+imageObjs[i][j].ystart);
+			selectYstart(i,j);
+		/*if(i==0)
+				imageObjs[i][j].ystart=selectYstart(i,j)-imageObjs[i][j].height+20;
+			else{
+			ystart+=gapsshelf+10;
+			imageObjs[i][j].ystart=selectYstart(i,j)+(10*i)-imageObjs[i][j].height;//ystart+gapsshelves*i
+			}*/
+			console.log('ystart while draw '+imageObjs[i][j].ystart);
+			var pic = new Image();
+			pic.src = imageObjs[i][j].src;
+				
+			canvas.drawImage(pic,imageObjs[i][j].xfinal,imageObjs[i][j].ystart,imageObjs[i][j].width,imageObjs[i][j].height);
+			
+				}
+				}
+}
+
+function selectYstart(i,j){
+	var closest=99999,l=0;
+	
+	for(var k =0;k<shelvespos.length;k++)
+	{
+		var diff=Math.abs(shelvespos[k]-imageObjs[i][j].ystopped);
+		if(diff<closest)
+			{closest=diff;
+				l=k;
+			}
+	}
+	console.log('shelvespos '+shelvespos[l]);
+	imageObjs[i][j].row=l;
+	return shelvespos[l];
+}
+
 function manipulateImages(){
 wheretoplace();
 getShelfReady();
@@ -153,6 +193,7 @@ for(var i = 0 ; i <imageObjs.length; i++){
 		var gaps = (xend - totalwidth)/(imageObjs[i].length+1);
 		console.log('gaps '+gaps);
 		for(var j = 0 ; j < imageObjs[i].length;j++){
+			//imageObjs[i][j].ystart=	ystart+(gapsshelf*i)+10;
 			imageObjs[i][j].xfinal = imageObjs[i][j].xstart+(j+1)*gaps+20;
 			console.log('xfinal '+imageObjs[i][j].xfinal);
 		}
@@ -189,6 +230,7 @@ function getShelfReady(){
 	var firstRow=0;
 	imageObjs=[];
 	imageObjs.push([]);
+	var productPerRow=0;
 	//shelf.push(img);
 	var xs=0;
 	for(var i = 0 ; i < shelf.length;i++)
@@ -196,6 +238,8 @@ function getShelfReady(){
 		if(firstRow==shelf[i].row)
 			{
 				//console.log('true');
+				shelf[i].column=productPerRow;
+				productPerRow++;
 				shelf[i].xstart=xs;
 				xs=xs+shelf[i].width;
 				imageObjs[firstRow].push(shelf[i]);
@@ -204,10 +248,12 @@ function getShelfReady(){
 			//console.log('false');
 			firstRow++;
 			xs=0;
+			productPerRow=0;
 			shelf[i].xstart=xs;
 				xs=xs+shelf[i].width;
 			imageObjs.push([]);
 			imageObjs[firstRow].push(shelf[i])
+			productPerRow++;
 			
 		}		
 	}
